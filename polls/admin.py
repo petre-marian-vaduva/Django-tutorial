f79606b940de7317d3621a38f603fae81cdfd51e
Vaduva <petremarian.vaduva@dxc.com>
2022-02-01 16:25:45 +0000

    register models in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
+from tabnanny import verbose
 from django.contrib import admin
+from .models import Question, Choice
 
+
+admin.site.register(Question)
+admin.site.register(Choice)
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ import datetime
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, verbose_name='!!!!')
     pub_date = models.DateTimeField('Date published')
     def __str__(self):
         return self.question_text
@@ -18,4 +18,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.question_text
+        return self.choice_text
+
+
