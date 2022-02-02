8826ad6b4013d86c8f98466f788f8ceebc20e57d
Vaduva <petremarian.vaduva@dxc.com>
2022-02-02 22:00:01 +0000

    last commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='detail'),
-    path('<int:question_id>/results', views.results, name='results'),
-    path('<int:question_id>/vote', views.vote, name='vote')
+    path('<question_id>', views.detail, name='detail'),
+    path('<question_id>/results', views.results, name='results'),
+    path('<question_id>/vote', views.vote, name='vote'),
+    path('<int:slug>', views.detail_redirect, name='detail_redirect')
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,23 @@
+from ast import arg
+from ssl import HAS_TLSv1_1
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.all()
     return render(request, 'polls/index.html', {
         'lastest_question_list': latest_question_list
-    })
+        })
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question':question
+    })
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -18,3 +25,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail_redirect(request, slug):
+    question = Question.objects.get(pk=slug)
+    value = question.choice_set.all()
+    redirect_path = HttpResponseRedirect(reverse('detail_redirect', args=[value]))
